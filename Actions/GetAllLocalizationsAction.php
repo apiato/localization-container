fdf256322cf46f9f4608e17eb0ea6eebe447a8db
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-04-11 15:22:49 +0430

    Remove usage of apiato caller style and use FQCN instead

--- Actions/GetAllLocalizationsAction.php
+++ Actions/GetAllLocalizationsAction.php
@@ -2,14 +2,15 @@
 
 namespace App\Containers\VendorSection\Localization\Actions;
 
-use App\Ship\Parents\Actions\Action;
 use Apiato\Core\Foundation\Facades\Apiato;
+use App\Containers\VendorSection\Localization\Tasks\GetAllLocalizationsTask;
+use App\Ship\Parents\Actions\Action;
 use Illuminate\Support\Collection;
 
 class GetAllLocalizationsAction extends Action
 {
-    public function run(): Collection
-    {
-        return Apiato::call('Localization@GetAllLocalizationsTask');
-    }
+	public function run(): Collection
+	{
+		return Apiato::call(GetAllLocalizationsTask::class);
+	}
 }
--- UI/API/Controllers/Controller.php
+++ UI/API/Controllers/Controller.php
@@ -3,15 +3,16 @@
 namespace App\Containers\VendorSection\Localization\UI\API\Controllers;
 
 use Apiato\Core\Foundation\Facades\Apiato;
+use App\Containers\VendorSection\Localization\Actions\GetAllLocalizationsAction;
 use App\Containers\VendorSection\Localization\UI\API\Requests\GetAllLocalizationsRequest;
 use App\Containers\VendorSection\Localization\UI\API\Transformers\LocalizationTransformer;
 use App\Ship\Parents\Controllers\ApiController;
 
 class Controller extends ApiController
 {
-    public function getAllLocalizations(GetAllLocalizationsRequest $request): array
-    {
-        $localizations = Apiato::call('Localization@GetAllLocalizationsAction');
-        return $this->transform($localizations, LocalizationTransformer::class);
-    }
+	public function getAllLocalizations(GetAllLocalizationsRequest $request): array
+	{
+		$localizations = Apiato::call(GetAllLocalizationsAction::class);
+		return $this->transform($localizations, LocalizationTransformer::class);
+	}
 }
